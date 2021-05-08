cc7ceb64bfe41af355426039dd0c30e5156c452d
Calandra Stavroudis <calandra.st@gmail.com>
2021-05-08 14:05:20 -0400

    implemented google places autocomplete for selecting countries in add trip form

--- client/components/AddTrip.js
+++ client/components/AddTrip.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Container, Form, Col } from "react-bootstrap";
 import { addTrip } from "../store/trips";
+import { Autocomplete } from "@react-google-maps/api";
 
 const initalState = {
   title: "",
-  countries: "",
+  countries: [],
   date: "",
+  autocomplete: false,
+  lat: 0,
+  lng: 0,
+  address: "",
 };
 class AddTrip extends React.Component {
   constructor(props) {
@@ -15,9 +20,17 @@ class AddTrip extends React.Component {
       title: "",
       countries: [],
       date: "",
+      autocomplete: false,
+      lat: 0,
+      lng: 0,
+      address: "",
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.onPlaceChanged = this.onPlaceChanged.bind(this);
+    this.onLoad = this.onLoad.bind(this);
+    this.autocomplete = null;
+    this.addCountry = this.addCountry.bind(this);
   }
 
   handleChange(event) {
@@ -28,6 +41,36 @@ class AddTrip extends React.Component {
     event.preventDefault();
     this.props.addTrip(this.state);
     this.setState(initalState);
+  }
+
+  onLoad(autocomplete) {
+    this.autocomplete = autocomplete;
+  }
+
+  onPlaceChanged() {
+    if (this.autocomplete !== null) {
+      const place = this.autocomplete.getPlace();
+      this.setState({
+        lat: place.geometry.location.lat(),
+        lng: place.geometry.location.lng(),
+        address: place.formatted_address,
+      });
+      console.log("state after place clicked:", this.state);
+    } else {
+      console.log("Autocomplete is not loaded yet!");
+    }
+  }
+
+  async addCountry(event) {
+    console.log("event:", event);
+    await this.setState({
+      countries: [
+        ...this.state.countries,
+        [this.state.address, [this.state.lat, this.state.lng]],
+      ],
+    });
+    await this.setState({ address: "", lat: 0, lng: 0 });
+    console.log(this.state);
   }
 
   render() {
@@ -55,24 +98,28 @@ class AddTrip extends React.Component {
 
           <Form.Group controlId="formBasicCountries">
             <Form.Label>Country(s) Visited</Form.Label>
-            <Form.Text>{this.state.countries}</Form.Text>
-            <Form.Control
-              name="countries"
-              onChange={this.handleChange}
-              value={this.state.countries}
-            />
-            <Form.Text>Please separate by commas.</Form.Text>
+            {this.state.countries.map((country) => (
+              <Form.Text key={country[0]}>{country[0]}</Form.Text>
+            ))}
+
+            <Form.Row>
+              <Col>
+                <Autocomplete
+                  onLoad={this.onLoad}
+                  onPlaceChanged={this.onPlaceChanged}
+                >
+                  <Form.Control />
+                </Autocomplete>
+              </Col>
+              <Col>
+                <Button type="button" size="sm" onClick={this.addCountry}>
+                  Add Country
+                </Button>
+              </Col>
+            </Form.Row>
           </Form.Group>
 
           <Button type="submit">Add Trip</Button>
-          {/* <Button
-            onClick={(event) => {
-              event.preventDefault();
-              this.addNewTrip(trip);
-            }}
-          >
-            {this.loading ? "Loading..." : "Add Trip"}
-          </Button> */}
         </Form>
       </Container>
     );
